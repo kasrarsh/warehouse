d011129ff93689ead27418e34794f0cb1bf333bb
Kasra rashidfar <kasramagers1377@gmail.com>
2020-05-07 02:01:57 +0430

    all models has been created

--- warehousing/models.py
+++ warehousing/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    role = models.CharField(max_length = 200)
+    telePhone = models.IntegerField()
+    bank_account = models.IntegerField()
+class Entry(models.Model):
+    seller= models.ForeignKey(
+            Person,
+            on_delete = models.CASCADE
+            )
+    total_cost = models.IntegerField()
+    date = models.DateField()
+
+class Category(models.Model):
+    name = models.CharField(max_length = 200)
+    parent = models.ForeignKey(
+            'self',
+            on_delete=models.CASCADE,
+
+            )
+
+class Product(models.Model):
+    name = models.CharField(max_length= 200)
+    color= models.CharField(max_length= 200,blank = True)
+    buy_cost = models.IntegerField()
+    sell_cost = models.IntegerField(blank=True)
+    profit = models.IntegerField(blank=True)
+    category=models.ForeignKey(
+            'Category',
+            on_delete=models.CASCADE
+            )
+    count = models.IntegerField()
+    entries = models.ManyToManyField(
+                Entry,
+                )
+
+
+    
+
--- werehouse/settings.py
+++ werehouse/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'warehousing.apps.WarehousingConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
